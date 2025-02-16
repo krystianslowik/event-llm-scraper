66846c4f3e3e23d582a11a66cea71904f509d89e
krystianslowik <me@krystianslowik.com>
2025-02-16 23:58:06 +0100

    chore: cleanup

--- scraper_old/src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-function calculateMedian(numbers) {
-    if (!numbers.length) return 0
-    const sorted = [...numbers].sort((a, b) => a - b)
-    const mid = Math.floor(sorted.length / 2)
-    return sorted.length % 2 !== 0
-        ? sorted[mid]
-        : (sorted[mid - 1] + sorted[mid]) / 2
-}
